d2ae9ce7a17e0bc008fd8da4cb4667914e527054
Nabeel Boda <needtoexplore01@gmail.com>
2024-02-16 12:11:26 +0530

    Feat: Scroll to top

--- ui/src/components/home/Header.jsx
+++ ui/src/components/home/Header.jsx
@@ -1,9 +1,19 @@
+import { useRef } from "react";
 import SecondaryBtn from "../SecondaryBtn";
 
 export default function Header() {
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
   return (
     <header className="flex justify-between pt-5 items-center sticky top-0 py-5 z-20 bg-white md:px-20 px-2">
-      <h1 className="text-3xl font-semibold font-sans text-primary">
+      <h1
+        onClick={scrollToTop}
+        className="text-3xl font-semibold font-sans text-primary cursor-pointer"
+      >
         Indic Subtitler
       </h1>
       <SecondaryBtn />
