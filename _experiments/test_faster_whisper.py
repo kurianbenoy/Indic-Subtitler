103274a061b6c2533e73e217d7420bca3dd664d7
kurianbenoy <kurian.bkk@gmail.com>
2024-02-20 22:55:39 +0530

    fix pre-commmit issues

--- _experiments/test_faster_whisper.py
+++ _experiments/test_faster_whisper.py
@@ -5,9 +5,16 @@ model_size = "large-v3"
 # Run on GPU with FP16
 model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
-segments, info = model.transcribe("Bishop Thomas Tharayil speaks about Dr Shashi Tharoor at Lourdes Forane Church Thiruvananthapuram.mp4", beam_size=5, language="en")
+segments, info = model.transcribe(
+    "Bishop Thomas Tharayil speaks about Dr Shashi Tharoor at Lourdes Forane Church Thiruvananthapuram.mp4",
+    beam_size=5,
+    language="en",
+)
 
-print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+print(
+    "Detected language '%s' with probability %f"
+    % (info.language, info.language_probability)
+)
 
 for segment in segments:
-        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
