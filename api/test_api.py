a13a6a11995e13bc62e4e5c202b43c3913019c77
kurianbenoy <kurian.bkk@gmail.com>
2024-02-20 22:10:42 +0530

    remove source parameter

--- api/test_api.py
+++ api/test_api.py
@@ -57,7 +57,7 @@ URL = "https://kurianbenoy--seamless-m4t-speech-generate-seamlessm4t-speech.moda
 
 b64 = audio_file_to_base64("/home/kurian/git/Indic-Subtitler/api/mal_sample1.mp4")
 # print(b64)
-print({"wav_base64": b64, "source": "mal", "target": "ben"})
+print({"wav_base64": b64, "target": "mal"})
 x = requests.post(URL, json={"wav_base64": b64, "source": "mal", "target": "hin"})
 print(x)
 res = x.json()
