9528680cd92d23b4c75dc72774aaf7e03d9b3373
Nabeel Boda <needtoexplore01@gmail.com>
2024-02-25 02:18:27 +0530

    style: different UI for uploaded files

--- ui/src/pages/generate.js
+++ ui/src/pages/generate.js
@@ -6,9 +6,35 @@ import { ToastContainer, toast } from "react-toastify";
 import SubtitleEditor from "@components/components/SubtitleEditor";
 import useLocalStorage from "@components/hooks/useLocalStorage";
 import { useRouter } from "next/router";
-import { IconLink } from "@tabler/icons-react";
-import { getRequestParamsForModel } from "@components/utils";
-
+import {
+  IconCalendarMonth,
+  IconDatabase,
+  IconFileDatabase,
+  IconLanguage,
+  IconLink,
+  IconMovie,
+  IconRobotFace,
+} from "@tabler/icons-react";
+import {
+  formatFileSize,
+  formattedDate,
+  getFullLanguageName,
+  getRequestParamsForModel,
+  getYouTubeVideoId,
+} from "@components/utils";
+function FileDetail({ icon, label, value }) {
+  return (
+    <div className="flex gap-2">
+      {icon}
+      <p>
+        {label}: {value}
+      </p>
+    </div>
+  );
+}
+function Thumbnail({ image }) {
+  return <img className="max-h-72  w-full" src={image} alt="Thumbnail" />;
+}
 export default function dashboard() {
   const [uploadedFile, setUploadedFile] = useState();
   const [targetLanguage, setTargetLanguage] = useState();
@@ -19,6 +45,8 @@ export default function dashboard() {
   const [transcribed, setTranscribed] = useState([]);
   const [requestSentToAPI, setrequestSentToAPI] = useState(false);
   const [isLocalFile, setIsLocalFile] = useState(false);
+  // const [fileDetailsForLocal, setfileDetailsForLocal] = useState()
+  const [uploadedFileInformation, setuploadedFileInformation] = useState();
   const [isSubtitleBeingGenerated, setIsSubtitleBeingGenerated] =
     useState(false);
 
@@ -33,18 +61,19 @@ export default function dashboard() {
     const items = JSON.parse(localStorage.getItem("file"));
     if (index && items) {
       if (items[index]) {
+        setIsLocalFile(true);
         const item = items[index];
         setDisabled(true);
-        setUploadedFile(item.uploadedFile);
-        setTargetLanguage(item.targetLanguage);
         setTranscribed(item.transcribedData);
-        setUploadedFile({
-          path: item.filename,
-          size: item.size,
+        console.log(item);
+        setuploadedFileInformation({
+          filename: item.filename,
+          filesize: item.size,
+          targetLanguage: item.targetLanguage ?? item.outputLanguage,
+          uploadDate: item.uploadDate,
+          model: item.model,
+          link: item.link,
         });
-        setIsLocalFile(true);
-        setSelectedModel(item.model);
-        setTargetLanguage(item.targetLanguage ?? item.outputLanguage);
       }
     }
   }, [index]);
@@ -141,6 +170,7 @@ export default function dashboard() {
 
         const file = {
           filename: uploadedFile?.path ?? filename,
+          link: youtubeLink,
           size: uploadedFile?.size,
           transcribedData: transcription,
           uploadDate: new Date(),
@@ -165,60 +195,121 @@ export default function dashboard() {
   return (
     <>
       <ToastContainer />
-      <main className="flex flex-col md:flex-row md:mb-8 xl:mx-14 mx-4 gap-4">
-        <aside className="w-full md:w-[30%] lg:w-[25%] flex flex-col space-y-10 p-2">
-          <div>
-            <h2 className="text-3xl font-medium">Upload a File</h2>
-            <p className="font-xl text-gray-500 font-medium mt-2">
-              Upload an audio file to generate subtitles
-            </p>
-          </div>
-          <div className="h-60">
-            <Dropzone
-              setUploadedFile={setUploadedFile}
-              uploadedFile={uploadedFile}
-            />
-          </div>
 
-          <div className="divider font-medium">OR</div>
-          <label className="flex border-2 rounded-lg gap-2 p-2 ">
-            <IconLink color="grey" />
-            <input
-              onChange={handleInputChange}
-              type="text"
-              className="w-full outline-none "
-              placeholder="Paste YouTube Video Link"
-            />
-          </label>
-          <div className="space-y-5">
-            {/* <Dropdown
-              onChange={(item) => setSelectedModel(item)}
-              label="Model"
-              options={AVAILABLE_MODELS}
-              keyName="llm-model"
-              defaultOption="Select Model"
-            /> */}
-            <Dropdown
-              onChange={(item) => setTargetLanguage(item)}
-              label="Subtitle Language"
-              options={SOURCE_LANGUAGES}
-              keyName="target-language"
-              defaultOption="Select Language"
-              selectedModel={selectedModel}
-            />
-          </div>
-          <button
-            disabled={disabled}
-            onClick={handleSubmit}
-            className={` ${
-              disabled
-                ? "bg-gray-400 hover:cursor-not-allowed"
-                : "bg-primary-900 hover:cursor-pointer"
-            } text-white py-2 rounded-md text-lg font-medium transition-all duration-300 flex items-center justify-center`}
-          >
-            Generate
-          </button>
+      <main className="flex flex-col md:flex-row md:mb-8 xl:mx-14 mx-4 gap-4 ">
+        <aside className="w-full md:w-[30%] lg:w-[25%] flex flex-col space-y-10 p-2 ">
+          {isLocalFile ? (
+            <div className="h-full ">
+              <h2 className="text-3xl font-medium mb-5">File Information</h2>
+              <Thumbnail
+                image={
+                  uploadedFileInformation.link
+                    ? `https://img.youtube.com/vi/${getYouTubeVideoId(
+                        uploadedFileInformation.link
+                      )}/0.jpg`
+                    : "/audio-file.svg"
+                }
+              />
+              <div className="text-gray-600 space-y-5  mt-4">
+                <FileDetail
+                  icon={<IconMovie />}
+                  label="File Name"
+                  value={uploadedFileInformation.filename}
+                />
+                {uploadedFileInformation.link ? (
+                  <div className="flex gap-2">
+                    <IconLink />
+                    <p>
+                      Link:{" "}
+                      <a
+                        href={uploadedFileInformation.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:text-blue-600"
+                      >
+                        {uploadedFileInformation.link}
+                      </a>
+                    </p>
+                  </div>
+                ) : null}
+                <FileDetail
+                  icon={<IconLanguage />}
+                  label="Subtitle Language"
+                  value={getFullLanguageName(
+                    uploadedFileInformation.model ?? "seamlessM4t",
+                    uploadedFileInformation.targetLanguage
+                  )}
+                />
+
+                <FileDetail
+                  icon={<IconRobotFace />}
+                  label="Model Used"
+                  value={uploadedFileInformation.model}
+                />
+                {uploadedFileInformation.filesize && (
+                  <FileDetail
+                    icon={<IconDatabase />}
+                    label="File Size"
+                    value={formatFileSize(uploadedFileInformation.filesize)}
+                  />
+                )}
+                <FileDetail
+                  icon={<IconCalendarMonth />}
+                  label="Upload Date"
+                  value={formattedDate(uploadedFileInformation.uploadDate)}
+                />
+              </div>
+            </div>
+          ) : (
+            <>
+              <div>
+                <h2 className="text-3xl font-medium">Upload a File</h2>
+                <p className="font-xl text-gray-500 font-medium mt-2">
+                  Upload an audio file to generate subtitles
+                </p>
+              </div>
+              <div className="h-60">
+                <Dropzone
+                  setUploadedFile={setUploadedFile}
+                  uploadedFile={uploadedFile}
+                />
+              </div>
+
+              <div className="divider font-medium">OR</div>
+              <label className="flex border-2 rounded-lg gap-2 p-2 ">
+                <IconLink color="grey" />
+                <input
+                  onChange={handleInputChange}
+                  type="text"
+                  className="w-full outline-none "
+                  placeholder="Paste YouTube Video Link"
+                />
+              </label>
+              <div className="space-y-5">
+                <Dropdown
+                  onChange={(item) => setTargetLanguage(item)}
+                  label="Subtitle Language"
+                  options={SOURCE_LANGUAGES}
+                  keyName="target-language"
+                  defaultOption="Select Language"
+                  selectedModel={selectedModel}
+                />
+              </div>
+              <button
+                disabled={disabled}
+                onClick={handleSubmit}
+                className={` ${
+                  disabled
+                    ? "bg-gray-400 hover:cursor-not-allowed"
+                    : "bg-primary-900 hover:cursor-pointer"
+                } text-white py-2 rounded-md text-lg font-medium transition-all duration-300 flex items-center justify-center`}
+              >
+                Generate
+              </button>
+            </>
+          )}
         </aside>
+
         <SubtitleEditor
           isBeingGenerated={isSubtitleBeingGenerated}
           transcribed={transcribed}
