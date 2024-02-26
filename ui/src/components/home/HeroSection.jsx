51fa60300e892c738a319fef3bc4be635ca15b04
Nabeel Boda <needtoexplore01@gmail.com>
2024-02-26 16:28:40 +0530

    updated list of supported models

--- ui/src/components/home/HeroSection.jsx
+++ ui/src/components/home/HeroSection.jsx
@@ -6,6 +6,7 @@ import { transitionToCollection } from "@components/utils";
 import { useRouter } from "next/router";
 
 export default function HeroSection() {
+  const listOfSupportedModels = ["SeamlessM4T", "faster-whisper", "WhisperX"];
   const router = useRouter();
   return (
     <section className="md:mt-44 mt-16 flex  justify-between lg:justify-center lg:gap-56 items-center md:px-0 px-4">
@@ -15,8 +16,17 @@ export default function HeroSection() {
           <span className="text-primary-900">Indic Languages</span>
         </h1>
         <p className="text-gray-500 text-lg">
-          Supported by SeamlessM4T-v2 large model, which supports almost 12
-          Indic languages by default.
+          Supported by{" "}
+          {listOfSupportedModels.map((tech, index) => (
+            <React.Fragment key={index}>
+              {index === listOfSupportedModels.length - 1 ? " and " : null}
+              <span className="font-medium text-gray-700" key={index}>
+                {tech}
+              </span>
+              {index < listOfSupportedModels.length - 2 ? ", " : null}
+            </React.Fragment>
+          ))}{" "}
+          which support almost 12 Indic languages by default.
         </p>
         <div className="flex gap-4 flex-wrap">
           <PrimaryBtn fn={() => transitionToCollection(router)}>
