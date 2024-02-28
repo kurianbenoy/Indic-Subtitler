522cb84ed89ccb35547ab5d74ca449c54f967be6
Aldrin Jenson <aldrinjenson@gmail.com>
2024-02-29 02:31:03 +0530

    MVP Done for live transcription

--- ui/src/pages/livetranscribe.js
+++ ui/src/pages/livetranscribe.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 
 const LiveTranscribe = () => {
@@ -6,42 +6,69 @@ const LiveTranscribe = () => {
   const [isRecording, setIsRecording] = useState(false);
   const [transcription, setTranscription] = useState("");
   const mediaRecorderRef = useRef(null);
+  const streamRef = useRef(null);
+  const [enableTranscription, setEnableTranscription] = useState(false);
 
-  const startRecording = () => {
+  const toggleRecording = () => {
+    if (!streamRef.current || !mediaRecorderRef.current) {
+      console.log({ streamRef, mediaRecorderRef });
+      console.log("stream or mediarecorder not defined");
+      return;
+    }
+    console.log({ state: mediaRecorderRef.current.state, isRecording });
+    if (mediaRecorderRef.current.state === "recording") {
+      mediaRecorderRef.current.stop();
+    } else {
+      mediaRecorderRef.current.ondataavailable = handleDataAvailable;
+      mediaRecorderRef.current.start();
+    }
+    setIsRecording(mediaRecorderRef.current.state === "recording");
+  };
+
+  useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({ audio: true })
       .then((stream) => {
+        console.log("stream set");
+        streamRef.current = stream;
         const mediaRecorder = new MediaRecorder(stream);
         mediaRecorderRef.current = mediaRecorder;
-        mediaRecorder.ondataavailable = handleDataAvailable;
-        mediaRecorder.start();
-        setIsRecording(true);
       })
       .catch((error) => {
         console.error("Error accessing microphone:", error);
       });
-  };
+  }, []);
 
-  const stopRecording = () => {
-    if (
-      mediaRecorderRef.current &&
-      mediaRecorderRef.current.state !== "inactive"
-    ) {
-      mediaRecorderRef.current.stop();
-      setIsRecording(false);
+  useEffect(() => {
+    let interval = null;
+    if (!enableTranscription) {
+      interval && clearInterval(interval);
+      return;
     }
-  };
+
+    interval = setInterval(() => {
+      toggleRecording();
+      setTimeout(() => {
+        console.log("toggling back");
+        toggleRecording();
+      }, 275);
+    }, 3500);
+
+    return () => interval && clearInterval(interval);
+  }, [enableTranscription]);
 
   const handleDataAvailable = (event) => {
-    console.log("data vailabls");
     const audioBlob = event.data;
     console.log(event.data);
     const formData = new FormData();
     formData.append("file", audioBlob, "audio.wav");
     formData.append("model", "whisper-1");
     formData.append("model", "whisper-1");
+    formData.append("language", "en");
+    formData.append("response_format", "verbose_json");
     formData.append("prompt", transcription);
 
+    console.log("making transcribe request");
     axios
       .post("https://api.openai.com/v1/audio/transcriptions", formData, {
         headers: {
@@ -50,21 +77,46 @@ const LiveTranscribe = () => {
         },
       })
       .then((response) => {
-        setTranscription(transcription + response.data.text);
+        console.log(response.data.text);
+        setTranscription(
+          (transcription) => `${transcription} ${response.data.text}`
+        );
       })
       .catch((error) => {
         console.error("Error transcribing audio:" + error);
       });
   };
 
+  const handleEnableTranscription = () => {
+    setEnableTranscription((currState) => !currState);
+  };
+
   return (
-    <div>
-      <div>Transcription: {transcription}</div>
-      {!isRecording ? (
-        <button onClick={startRecording}>Start Recording</button>
-      ) : (
-        <button onClick={stopRecording}>Stop Recording</button>
-      )}
+    <div className="prose mx-auto">
+      <btn className="btn btn-accent" onClick={handleEnableTranscription}>
+        {enableTranscription ? "Disable" : "Enable"} Transcription
+      </btn>
+
+      <h1>Transcription</h1>
+      <div className="h-80">
+        {/* <textarea rows={10} cols={20} className="w-10"> */}
+        {transcription}
+        {/* </textarea> */}
+      </div>
+      <br />
+
+      <p>{isRecording ? "Recording in progress...." : "Ready"} </p>
+
+      <button className="btn btn-outline" onClick={toggleRecording}>
+        {isRecording ? "Stop Recording" : "Start Recording"}
+      </button>
+
+      <button
+        className="btn btn-error mx-5"
+        onClick={() => setTranscription("")}
+      >
+        Clear Transcript
+      </button>
     </div>
   );
 };
