cc15565def4ec190dc1bcc2fd4c4bb71156d3522
Aldrin Jenson <aldrinjenson@gmail.com>
2024-03-09 18:04:53 +0530

    Add vercel analytics

--- ui/src/pages/_app.js
+++ ui/src/pages/_app.js
@@ -3,15 +3,19 @@ import "@components/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Indic Subtitle - Generate Subtitle</title>
+        <title>
+          Indic Subtitler - Open Source Subtitling tool for Indic Languages!
+        </title>
       </Head>
       <Header />
       <Component {...pageProps} />
+      <Analytics />
       <ToastContainer />
     </>
   );
