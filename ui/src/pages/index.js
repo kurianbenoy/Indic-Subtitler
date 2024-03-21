4e0a16513a8ff696b2edf2d03fd11b828f8afa50
Aldrin Jenson <aldrinjenson@gmail.com>
2024-03-22 00:21:33 +0530

    Add better meta tags

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -1,5 +1,4 @@
 import Footer from "../components/home/Footer";
-import Header from "../components/Header";
 import HeroSection from "../components/home/HeroSection";
 import Information from "../components/home/Information";
 import Head from "next/head";
@@ -10,6 +9,35 @@ export default function Home() {
     <main className="bg-white">
       <Head>
         <title>Indic Subtitler - Home</title>
+        <meta name="title" content="Indic Subtitler - Home" />
+        <meta
+          name="description"
+          content="Open Source subtitle generator for Indic-Languages | Use powerful ML models to transcribe and transalte audio and video in multipe languages "
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://indicsubtitler.in/" />
+        <meta property="og:title" content="Indic Subtitler - Home" />
+        <meta
+          property="og:description"
+          content="Open Source subtitle generator for Indic-Languages | Use powerful ML models to transcribe and transalte audio and video in multipe languages "
+        />
+        <meta
+          property="og:image"
+          content="https://i.postimg.cc/X7r6J7WM/indicsubtitler.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://indicsubtitler.in/" />
+        <meta property="twitter:title" content="Indic Subtitler - Home" />
+        <meta
+          property="twitter:description"
+          content="Open Source subtitle generator for Indic-Languages | Use powerful ML models to transcribe and transalte audio and video in multipe languages "
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.postimg.cc/X7r6J7WM/indicsubtitler.png"
+        />
       </Head>
       <HeroSection />
       <Information />
