170ec4f59c6db18b11ed9a415fa767530c6ec9aa
kurianbenoy <kurian.bkk@gmail.com>
2024-03-26 22:00:14 +0530

    add headers

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
 import SecondaryBtn from "./SecondaryBtn";
-import { useEffect, useState } from "react";
-import { transitionToCollection } from "@components/utils";
 import PrimaryBtn from "./PrimaryBtn";
+import { transitionToCollection } from "@components/utils";
 
 function Buttons({ isHome }) {
   const router = useRouter();
+
   if (isHome) {
     return (
       <div className="flex flex-wrap gap-4">
@@ -13,14 +13,15 @@ function Buttons({ isHome }) {
           Team
         </PrimaryBtn>
         <PrimaryBtn accent={true} fn={() => router.push("/livetranscribe")}>
-          Live Transcription
+          Transcribe Now
         </PrimaryBtn>
         <SecondaryBtn fn={() => transitionToCollection(router)}>
-          Generate Subtitles
+          Get Subtitles
         </SecondaryBtn>
       </div>
     );
   }
+
   if (router.pathname !== "/contact") {
     return (
       <div className="max-w-fit">
@@ -28,10 +29,14 @@ function Buttons({ isHome }) {
       </div>
     );
   }
+
+  return null;
 }
+
 export default function Header() {
   const router = useRouter();
   const isHome = router.pathname === "/";
+
   function scrollToTop() {
     if (isHome) {
       window.scrollTo({
@@ -46,15 +51,17 @@ export default function Header() {
   return (
     <header
       className={`
-    ${isHome && "top-0"}
-    flex md:flex-row flex-col space-y-4 flex-wrap md:space-y-0 justify-between pt-5 md:items-center sticky  py-5 z-20 bg-white lg:px-20 px-2 md:gap-2 `}
+        ${isHome ? "top-0" : ""}
+        flex flex-col md:flex-row space-y-4 md:space-y-0 flex-wrap justify-between 
+        pt-5 md:items-center sticky py-5 z-20 bg-white px-2 lg:px-20 md:gap-2
+      `}
     >
       <div
         onClick={scrollToTop}
         className="flex items-center cursor-pointer gap-3"
       >
         <img src="/logo.png" width={70} alt="Logo of Indic Subtitler" />
-        <h1 className="text-3xl font-semibold font-sans text-primary-900 ">
+        <h1 className="text-3xl font-semibold font-sans text-primary-900">
           Indic Subtitler
         </h1>
       </div>
